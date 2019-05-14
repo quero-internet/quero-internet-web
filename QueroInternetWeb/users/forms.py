f2c0634ed05c442c76c4f8e73c6a3875a2aa7e1f
Igor Bueno <igor_mcb@hotmail.com>
2019-05-14 19:51:48 -0400

    Correção do cadastro de usuários no campo is_staff

--- QueroInternetWeb/users/forms.py
+++ QueroInternetWeb/users/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    is_staff = forms.BooleanField(label="Desejo ser um parceiro.")
+    is_staff = forms.BooleanField(label="Desejo ser um parceiro.", required=False)
     username = forms.CharField(label = "Nome de usuario")
     password1 = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(widget=forms.PasswordInput)
@@ -20,6 +20,7 @@ class RegistrationForm(UserCreationForm):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
+        user.is_staff
 
         if commit:
             user.save()
