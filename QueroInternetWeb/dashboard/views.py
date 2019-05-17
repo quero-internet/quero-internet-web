b978fab534c6e288f52ed751d8fe6a4b1dce5849
Igor Bueno <igor_mcb@hotmail.com>
2019-05-17 01:01:41 -0400

    Dashboard pronta

--- QueroInternetWeb/dashboard/views.py
+++ QueroInternetWeb/dashboard/views.py
@@ -7,6 +7,9 @@ from django.views.generic.base import TemplateView
 from django.shortcuts import redirect
 from main.models import Parceiro, Solicitacao
 from datetime import date
+import json
+from django.db.models import Sum, Count
+from django.core import serializers
 
 # Create your views here.
 @method_decorator(staff_member_required, name='dispatch')
@@ -19,6 +22,9 @@ class Dashboard(TemplateView):
         context['solicitacoesMes'] = self.get_numero_solicitacoes_mes()
         context['solicitacoesHoje'] = self.get_numero_solicitacoes_hoje()
         context['solicitacoesAno'] = self.get_numero_solicitacoes_ano()
+        context['quantitativo_por_situacao_data'] = self.get_quantitativo_por_situacao_data()
+        context['quantitativo_respondido_data'] = self.get_quantitativo_respondido_data()
+        context['quantitativo_conversoes_por_data'] = self.get_quantitativo_conversoes_por_data()
         return context
 
     def get_numero_solicitacoes_mes(self):
@@ -32,3 +38,22 @@ class Dashboard(TemplateView):
     def get_numero_solicitacoes_ano(self):
         today = date.today()
         return Solicitacao.objects.filter(data_e_hora__year=today.year).count()
+
+    def get_quantitativo_por_situacao_data(self):
+        fechadas = Solicitacao.objects.filter(parceiro_escolhido__isnull = False).count()
+        abertas = Solicitacao.objects.filter(parceiro_escolhido__isnull = True).count()
+        
+        return json.dumps([abertas, fechadas])
+
+    def get_quantitativo_respondido_data(self):
+        respondido = Solicitacao.objects.filter(respostas__isnull = False).count()
+        respondido_convertido = Solicitacao.objects.filter(respostas__isnull = False, parceiro_escolhido__pk=self.request.user.parceiro.pk).count()
+        nao_respondido = Solicitacao.objects.filter(respostas__isnull = True).count()
+        
+        return json.dumps([nao_respondido, respondido, respondido_convertido])
+
+    def get_quantitativo_conversoes_por_data(self):
+        qs = Solicitacao.objects.filter(parceiro_escolhido__pk=self.request.user.parceiro.pk)
+        qs = qs.extra(select={'data_e_hora':'DATE(data_e_hora)'})
+        qs = qs.values('data_e_hora').annotate(Count('pk'))
+        return json.dumps(list(qs))
